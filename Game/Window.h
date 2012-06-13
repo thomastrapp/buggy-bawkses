b0e09bef8e80f0656c883859c8e90c37132246c4
thomas <thomas@c5d34afc-0a70-4961-9e11-dfff0985a225>
2012-06-13 02:10:20 +0000

    Added documentation to Window

--- Game/Window.h
+++ Game/Window.h
@@ -10,43 +10,59 @@
 
 namespace Game
 {
-  class Window : public sf::RenderWindow
-  {
-  public:
-    Window(boost::shared_ptr<Game::Config> conf)
-    : sf::RenderWindow
+/**
+ * @brief A wrapper for sf::RenderWindow
+ */
+class Window : public sf::RenderWindow
+{
+public:
+  /**
+   * @brief Construct a Window
+   *
+   * Respects configuration options: window-width and window-height.
+   * Sets up a sf::View.
+   *
+   * @param conf A shared_ptr to Game::Config, a class that stores user supplied
+   * configuration, as well as internal key/value pairs.
+   */
+  Window(boost::shared_ptr<Game::Config> conf)
+  : sf::RenderWindow
+    (
+      sf::VideoMode
       (
-        sf::VideoMode
-        (
-          static_cast<unsigned int>(conf->get<int>("window-width")), 
-          static_cast<unsigned int>(conf->get<int>("window-height"))
-        ),
-        conf->get<std::string>("window-title")
+        static_cast<unsigned int>(conf->get<int>("window-width")), 
+        static_cast<unsigned int>(conf->get<int>("window-height"))
       ),
-      config(conf)
-    {
-      sf::View view(
-        sf::FloatRect(
-          0.0f, 
-          0.0f, 
-          static_cast<float>(conf->get<int>("window-width")),
-          static_cast<float>(conf->get<int>("window-height"))
-        )
-      );
-      this->setView(view);
-      this->setVerticalSyncEnabled(true);
-    }
+      conf->get<std::string>("window-title")
+    ),
+    config(conf)
+  {
+    sf::View view(
+      sf::FloatRect(
+        0.0f, 
+        0.0f, 
+        static_cast<float>(conf->get<int>("window-width")),
+        static_cast<float>(conf->get<int>("window-height"))
+      )
+    );
+    this->setView(view);
+    this->setVerticalSyncEnabled(true);
+  }
+
+  ~Window()
+  {
+  }
+
+private:
+  Window(Game::Window const&);
+  Window& operator=(Game::Window const&);
   
-    ~Window()
-    {
-    }
-  
-  private:
-    Window(Game::Window const&);
-    Window& operator=(Game::Window const&);
-    
-    boost::shared_ptr<Game::Config> config;
-  };
+  /**
+   * @brief A shared_ptr to Game::Config, a class that stores user supplied
+   * configuration, as well as internal key/value pairs.
+   */
+  boost::shared_ptr<Game::Config> config;
+};
 }
 
 #endif // GAMEWINDOW_H
