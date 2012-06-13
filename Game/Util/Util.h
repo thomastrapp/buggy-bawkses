9977f47f2f38a475c08c97b1fb21e228b813c2bc
thomas <thomas@c5d34afc-0a70-4961-9e11-dfff0985a225>
2012-06-13 01:02:32 +0000

    Added documentation to Util

--- Game/Util/Util.h
+++ Game/Util/Util.h
@@ -10,12 +10,35 @@ namespace Game
 {
   namespace Util
   {
+    /**
+     * @brief A large enough pie
+     */
     static const float pi = 3.14f;
+    
+    /**
+     * @brief Scale between Box2d (meters) and SFML (pixels)
+     */
     static const float pixel_per_meter = 128.0f;
   
+    /**
+     * @brief Convert pixels to meters
+     * @param pixels
+     * @return meters
+     */
     float pixel_to_meter(const float pixels);
+    
+    /**
+     * @brief Converts meters to pixels
+     * @param meters
+     * @return pixels
+     */
     float meter_to_pixel(const float meters);
     
+    /**
+     * @brief Convert radians to degrees
+     * @param rad radians
+     * @return degrees
+     */
     float rad_to_deg(const float rad);
   }
 }
